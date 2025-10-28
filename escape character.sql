64e014b49ceb3800bacda6975fe5a5445b80dd0a
nurhayat <nhayatt999@gmail.com>
2025-10-28 19:32:29 +0300

    Add files via upload

---
+++ escape character.sql
@@ -0,0 +1,11 @@
+-- escape characters
+
+select 'computer engineer';
+
+select 'I''m a computer engineer';
+
+select E'I\'m a computer engineer';
+
+$tag$<string_constant>$tag$
+
+select $$'I'm a computer engineer'$$;
