e808556d6f1bb3220f52f2c5a83ee2657e9c8897
nurhayat <nhayatt999@gmail.com>
2025-10-29 12:34:49 +0300

    ORDER BY Clause

--- order by .sql
+++ order by .sql
@@ -1,3 +1,5 @@
+--   Practice sorting data using the ORDER BY clause with single and multiple columns.
+
 -- ORDER BY SINGLE COLUMN
 SELECT * FROM film
 ORDER BY title
@@ -5,7 +7,7 @@ ORDER BY title
 SELECT district, address, city_id
 FROM address
 ORDER BY district
--- ORDER BY address_id (we can also sort by column we dont wat to see on screen)
+-- You can also sort by a column not included in the SELECT list
 
 SELECT first_name, last_name, email, address_id
 FROM customer
@@ -23,7 +25,13 @@ ORDER BY first_name asc, last_name asc
 -- ORDER BY EXTRA COST
 
 -- ORDER BY NULLS FIRST, LAST
+  -- 🔹 Sorting NULL values explicitly
 SELECT * FROM employees
 ORDER BY phone_number NULLS FIRST 
+
+-- 💡 Lesson Learned:
+--   - ORDER BY controls the sorting order of results.
+--   - DESC and ASC define descending or ascending order.
+--   - Sorting by multiple columns improves data organization and clarity.
 
 
